e5afb043b1874243389ba4ade33af01a98325dd4
mtolrom <SDE2013@outlook.com>
2018-10-04 00:58:21 -0700

    Adding Tabs layout

---
+++ app/src/main/java/com/example/yhussein/myapplication/LibraryFragment.java
@@ -0,0 +1,19 @@
+package com.example.yhussein.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Provides UI for the view with Cards.
+ */
+public class LibraryFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.item_library, null);
+    }
+}
