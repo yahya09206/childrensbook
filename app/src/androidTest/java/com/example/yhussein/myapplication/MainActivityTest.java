928ed7d9bc12f48319e15f3053319f8d45f6ce95
mtolrom <SDE2013@outlook.com>
2018-11-12 19:29:44 -0800

    Pushing Next and Play click Tests

--- app/src/androidTest/java/com/example/yhussein/myapplication/MainActivityTest.java
+++ app/src/androidTest/java/com/example/yhussein/myapplication/MainActivityTest.java
@@ -142,6 +142,44 @@ public class MainActivityTest {
     }
 
     @Test
+    public void TestNextChange(){
+        onView(withIndex(withId(R.id.book_img_id), 0)).perform(click());
+        onView(withIndex(withId(R.id.next), 0)).perform(click());
+
+        Intent intent = new Intent();
+        intent.putExtra("Id", 1);
+        intent.putExtra("Sound", "On");
+        intent.putExtra("Section", 0);
+        intent.putExtra("Language", "english");
+        intent.putExtra("Bookmark", 1);
+
+        Instrumentation.ActivityResult result =
+                new Instrumentation.ActivityResult(MainActivity.RESULT_OK, intent);
+
+        onView(withId(R.id.book_img_id)).check(matches(isDisplayed()));
+
+    }
+
+    @Test
+    public void TestPlayChange(){
+        onView(withIndex(withId(R.id.book_img_id), 0)).perform(click());
+        onView(withIndex(withId(R.id.play), 0)).perform(click());
+
+        Intent intent = new Intent();
+        intent.putExtra("Id", 1);
+        intent.putExtra("Sound", "On");
+        intent.putExtra("Section", 0);
+        intent.putExtra("Language", "english");
+        intent.putExtra("Bookmark", 1);
+
+        Instrumentation.ActivityResult result =
+                new Instrumentation.ActivityResult(MainActivity.RESULT_OK, intent);
+
+        onView(withId(R.id.book_img_id)).check(matches(isDisplayed()));
+
+    }
+
+    @Test
     public void TestBookEntity() throws Exception {
         Setting setting = new Setting(1, "Great Book Title 1", "Mekone Tolrom", "mekone",
                 "0", "english", "On",
